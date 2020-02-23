b691e4e277bd62d7705ccd080d2dbdf6ebe96c6b
Artur Symanovic <artur.symanovic@gmail.com>
2020-02-23 12:20:25 +0000

    Calculation of likes

---
+++ Playground/FacebookLikes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground
+{
+    class FacebookLikes
+    {
+        public List<string> Likes { get; set; }
+        
+    }
+}
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Playground
 {
@@ -6,7 +7,37 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            List<string> friends = new List<string>();
+            string name;
+            Console.WriteLine(@"Please enter a name of the person that liked the post.");
+            Console.WriteLine(@"If no one liked the post yet press enter to finish.");
+            while ((name = Console.ReadLine()) != "" )
+            {
+                friends.Add(name);
+                Console.WriteLine(@"Please enter a name of the person that liked the post.");
+                Console.WriteLine(@"If no one else liked the post press enter to show the result and finish.");
+            }
+            switch (friends.Count)
+            {
+                case 0:
+                    break;
+                case 1:
+                    Console.WriteLine(string.Format("{0} likes your post."
+                                                    ,friends[0]));
+                    break;
+                case 2:
+                    Console.WriteLine(string.Format("{0} and {1} like your post."
+                                                    ,friends[0]
+                                                    ,friends[1]));
+                    break;
+                default:
+                    Console.WriteLine(string.Format("{0}, {1} and {2} others like your post."
+                                                    ,friends[0]
+                                                    ,friends[1]
+                                                    ,friends.Count -2));
+                    break;
+            }
+            Console.WriteLine();
         }
     }
 }
