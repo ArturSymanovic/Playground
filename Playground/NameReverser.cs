0a1f15bbdccb7a78636cdc40f6b755f5380de86a
Artur Symanovic <artur.symanovic@gmail.com>
2020-02-23 13:40:59 +0000

    Added file reverser class

---
+++ Playground/NameReverser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground
+{
+    class NameReverser
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Please enter your name (type \"exit\" to finish):");
+            var name = Console.ReadLine();
+            while (name.Length <= 0)
+            {
+                Console.WriteLine("You did not enter a name! Try again!");
+                name = Console.ReadLine();
+            }
+            if (name.ToLower() == "exit") return;
+            char[] array = new char[name.Length];
+            for (int i = 0; i < name.Length; i++)
+            {
+                array[i] = name[(name.Length - 1) - i];
+            }
+            var reversedName = new string(array);
+            Console.WriteLine(string.Format("The reversed name of {0} is {1}", name, reversedName));
+        }
+    }
+}
